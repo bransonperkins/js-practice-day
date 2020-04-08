4d64cdfd6d72f223bb03483337ef7391df35a8de
Branson Perkins <bransonperkins@gmail.com>
2020-04-08 09:57:45 -0400

    completed pizza exercise

---
+++ pizza.js
@@ -0,0 +1,6 @@
+function pizza(){
+    let toppings = ['Cheese', 'Pepperoni', 'Garlic', 'Peppers'];
+    return "A delicious pizza with " + toppings[0] + " and " + toppings[1] + " and " + toppings[2] + " and " + toppings[3] + ".";
+};
+
+console.log(pizza());
