c50b7cc6c314c7e11a4846828bcfbf129501e3e7
Branson Perkins <bransonperkins@gmail.com>
2020-04-18 09:45:40 -0400

    simplified sandwich calculator and added .gitignore file

--- sandwichCalculator.js
+++ sandwichCalculator.js
@@ -1,17 +1,22 @@
-function sandwichCalculator(slicesOfBread, slicesOfCheese){
-    var sandwiches = Math.floor(slicesOfBread / 2);
-
-    if ((slicesOfBread > 1) && (slicesOfCheese > 0) && (
-        slicesOfCheese < sandwiches)) {
-            sandwiches = slicesOfCheese;
-            console.log(sandwiches);
-            return sandwiches;
-        } else if ((slicesOfCheese >= sandwiches) && (slicesOfBread > 1)) {
-            console.log(sandwiches);
-            return sandwiches;
-        } else if ((slicesOfCheese = 0) || (slicesOfBread < 2)) {
-            console.log("Oops, no sandwiches for you.");
-            return 0;
-        }
+// create a sandwich calculator that tells you how many sandwiches you can make based off of how much bread and cheese you have
+function sandwichCalculator(breadSlices, cheeseSlices){
+    var sandwiches = Math.floor(breadSlices / 2);
+    if ((breadSlices >= 2) && (cheeseSlices >= 1)) {
+        console.log(sandwiches);
+        return sandwiches;
+    } else if ((cheeseSlices == 0) || (breadSlices < 2)) {
+        console.log('No sandwiches for you.');
+        return 0;
+    }
 }
-sandwichCalculator(10,5);
+
+// Test Cases
+// Make sandwiches even when there is a surplus of cheese
+sandwichCalculator(2,100); // 1
+sandwichCalculator(6,3); // 3
+
+// No sandwiches made when there is no cheese
+sandwichCalculator(2,0); // 'No sandwiches for you'
+
+// No sandwiches made when there is not enough bread to make a sandwich
+sandwichCalculator(1,2); // 'No sandwiches for you'
